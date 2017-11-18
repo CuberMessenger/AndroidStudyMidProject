e8a4ac47ca0a4907cf65c2364494a9ac805b992c
CuberMessenger <cuber_messenger@hotmail.com>
2017-11-18 17:10:26 +0800

    auto suggest box

--- app/src/main/java/com/messenger/cuber/androidstudymidproject/MainActivity.kt
+++ app/src/main/java/com/messenger/cuber/androidstudymidproject/MainActivity.kt
@@ -2,13 +2,31 @@ package com.messenger.cuber.androidstudymidproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val test: Array<String> = arrayOf("张飞", "赵云", "关羽", "张角", "张姓男子", "马云", "马超")
+
+    private fun initListeners() {
+        searchButton.setOnClickListener {
+            val s: String = searchAutoSuggestTextView.text.toString()
+            testTV.text = s + "\t" + test.find { ts -> ts == s }.toString()
+        }
+    }
+
+    private fun init() {
+        initListeners()
+        searchAutoSuggestTextView.setAdapter(ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, test))
+        searchAutoSuggestTextView.threshold=1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        init()
     }
+
+
 }
---
+++ app/src/main/java/com/messenger/cuber/androidstudymidproject/Person.kt
@@ -0,0 +1,6 @@
+package com.messenger.cuber.androidstudymidproject
+
+/**
+ * Created by cuber on 2017/11/18.
+ */
+data class Person(val imgPath: String, val name: String, val sex: String, val bdData: String, val homeTown: String, val master: String)
